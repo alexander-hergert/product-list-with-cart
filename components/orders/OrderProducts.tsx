c6733826a2ef1d80f1689d248c26479c260381ed
Alexander Hergert <alexander.hergert1989@gmail.com>
2025-07-03 12:33:45 +0200

    Disable the Link to leave feedback if feedback is already given in that order.

--- components/orders/OrderProducts.tsx
+++ components/orders/OrderProducts.tsx
@@ -32,43 +32,70 @@ const OrderProducts: FC<OrderProductsProps> = async ({
   } finally {
     await prisma.$disconnect();
   }
+  //find feedbacks for the products in the order
+  let feedbacks: { productId: string }[] = [];
+  try {
+    feedbacks = await prisma.feedbacks.findMany({
+      where: {
+        orderId: orderId,
+        productId: { in: productIds },
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching feedbacks:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+  // Check if a product has feedback
+  const hasFeedbackForProduct = (productId: string) =>
+    feedbacks.some((f) => f.productId === productId);
 
   return (
     <div>
-      {products.map((product) => (
-        <div key={product.id}>
-          <Link href={`/dashboard/products/${product.id}`}>
-            <div
-              key={product.id}
-              className="border rounded-xl flex items-center justify-between 
-              max-md:flex-col max-md:m-auto max-md:w-[327px] shadow-md hover:shadow-lg transition-shadow duration-200"
-            >
-              <Image
-                className="md:rounded-l-xl max-md:w-full max-md:rounded-t-xl"
-                src={product.image}
-                alt={product.name}
-                width={250}
-                height={250}
-              />
-              <div className="m-auto max-md:m-0 max-md:my-4">
-                <h2 className="text-xl">{product.name}</h2>
-                {productIds && (
-                  <p>
-                    Quantity:{" "}
-                    {productIdsQuantity?.[productIds.indexOf(product.id)]}
-                  </p>
-                )}
-                <p>Price: ${product.price.toFixed(2)}</p>
+      {products.map((product) => {
+        const hasFeedback = hasFeedbackForProduct(product.id);
+
+        return (
+          <div key={product.id}>
+            <Link href={`/dashboard/products/${product.id}`}>
+              <div
+                className="border rounded-xl flex items-center justify-between 
+                max-md:flex-col max-md:m-auto max-md:w-[327px] shadow-md hover:shadow-lg transition-shadow duration-200"
+              >
+                <Image
+                  className="md:rounded-l-xl max-md:w-full max-md:rounded-t-xl"
+                  src={product.image}
+                  alt={product.name}
+                  width={250}
+                  height={250}
+                />
+                <div className="m-auto max-md:m-0 max-md:my-4">
+                  <h2 className="text-xl">{product.name}</h2>
+                  {productIds && (
+                    <p>
+                      Quantity:{" "}
+                      {productIdsQuantity?.[productIds.indexOf(product.id)]}
+                    </p>
+                  )}
+                  <p>Price: ${product.price.toFixed(2)}</p>
+                </div>
               </div>
-            </div>
-          </Link>
-          <Link href={`/dashboard/orders/${orderId}/${product.id}`}>
-            <button className="border rounded p-2 my-8 hover:bg-black hover:text-white block m-auto">
-              Leave Feedback...
-            </button>
-          </Link>
-        </div>
-      ))}
+            </Link>
+            {hasFeedback && (
+              <div className="rounded p-2 my-8 block m-auto border-4 w-1/3 text-center text-gray-300">
+                Leave Feedback...
+              </div>
+            )}
+            {!hasFeedback && (
+              <Link href={`/dashboard/orders/${orderId}/${product.id}`}>
+                <button className="border w-1/3 rounded p-2 my-8 hover:bg-black hover:text-white block m-auto">
+                  Leave Feedback...
+                </button>
+              </Link>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
