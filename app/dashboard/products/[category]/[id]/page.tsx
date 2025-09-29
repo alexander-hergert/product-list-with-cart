dbcce0576499b2210882001dbffa8cee0afca2c0
Alexander Hergert <alexander.hergert1989@gmail.com>
2025-09-29 18:00:16 +0200

    Add category description and new styles for /products/category/id and dashboard/products/category/id.

--- app/dashboard/products/[category]/[id]/page.tsx
+++ app/dashboard/products/[category]/[id]/page.tsx
@@ -41,33 +41,48 @@ const ProductDetailsPage = async ({ params }: { params: Params }) => {
   return (
     <div>
       <h1 className="text-2xl my-4 text-center font-bold">Product Details</h1>
-      <div className="flex flex-col items-center border rounded-xl p-4 shadow-md md:min-w-[800px] md:w-1/3 m-auto max-md:w-[80%]">
-        <div>
+      <section className="flex justify-center flex-col items-center border rounded-xl p-4 shadow-md md:min-w-[800px] md:w-1/3 m-auto max-md:w-[80%]">
+        <div className="flex max-md:gap-4 gap-8 max-lg:flex-col">
           <Image
             className="rounded-xl m-auto"
             src={product ? product.image : ""}
             alt={product ? product.name : ""}
-            width={300}
-            height={300}
+            width={400}
+            height={400}
           />
-          <div className="flex gap-4 font-bold items-center max-md:flex-col text-center my-4">
-            <label>Name:</label>
-            <h2>{product?.name}</h2>
-          </div>
-          <div className="flex gap-4 max-md:flex-col text-center my-4">
-            <label>Description:</label>
-            <p>{product?.description}</p>
-          </div>
-          <div className="flex gap-4 items-center max-md:flex-col text-center my-4">
-            <label>Price:</label>
-            <p>${product?.price}</p>
-          </div>
-          <div className="flex gap-2">
-            <label htmlFor="rating">Rating</label>
-            <Rating name="rating" value={product?.rating} readOnly />
-          </div>
+          <aside>
+            <div className="flex gap-4 items-center max-lg:flex-col text-center my-4 min-xl:w-[300px]">
+              <label className="font-bold">Name:</label>
+              <h2>{product?.name}</h2>
+            </div>
+            <div className="flex gap-4 items-center max-lg:flex-col text-center my-4 min-lg:w-[300px]">
+              <label className="font-bold" htmlFor="mainCategory">
+                Main Category:
+              </label>
+              <p>{product?.main_category}</p>
+            </div>
+            <div className="flex gap-4 items-center max-lg:flex-col text-center my-4 min-lg:w-[300px]">
+              <label className="font-bold" htmlFor="subCategory">
+                Sub Category:
+              </label>
+              <p>{product?.sub_category}</p>
+            </div>
+            <div className="flex gap-4 max-lg:flex-col max-lg:text-center my-4 min-lg:w-[300px]">
+              <label className="font-bold">Description:</label>
+              <p>{product?.description}</p>
+            </div>
+            <div className="flex gap-4 items-center max-lg:flex-col text-center my-4 min-lg:w-[300px]">
+              <label className="font-bold">Price:</label>
+              <p>${product?.price}</p>
+            </div>
+            <div className="flex gap-4 items-center max-lg:flex-col text-center my-4 min-lg:w-[300px]">
+              <label htmlFor="rating" className="font-bold">
+                Rating
+              </label>
+              <Rating name="rating" value={product?.rating} readOnly />
+            </div>
+          </aside>
         </div>
-
         <Link
           href={`/dashboard/products/${product?.main_category.toLocaleLowerCase()}/${id}/edit_product`}
           className="border rounded p-2 my-2 hover:bg-blue-700 hover:text-white self-center w-[95%] text-center"
@@ -75,7 +90,7 @@ const ProductDetailsPage = async ({ params }: { params: Params }) => {
           Edit
         </Link>
         <DeleteProduct id={id} />
-      </div>
+      </section>
       <Link
         className="border block w-[20%] max-md:w-[50%] m-auto mt-4 rounded p-2 my-2 hover:bg-blue-700 hover:text-white self-center text-center"
         href="/dashboard/products"
--- app/products/[category]/[id]/page.tsx
+++ app/products/[category]/[id]/page.tsx
@@ -86,51 +86,69 @@ const ProductsDetailsPage = async ({ params }: { params: Params }) => {
   return (
     <div>
       <h1 className="text-2xl my-4 text-center font-bold">Product Details</h1>
-      <div className="flex flex-col items-center border rounded-xl p-4 shadow-md md:min-w-[800px] md:w-1/3 m-auto max-md:w-[80%]">
-        <div>
+      <section className="flex flex-col items-center border rounded-xl p-4 shadow-md md:min-w-[800px] md:w-1/3 m-auto max-md:w-[80%]">
+        <div className="flex max-md:gap-4 gap-8 max-lg:flex-col">
           <Image
             className="rounded-xl m-auto"
             src={product.image}
             alt={product.name}
-            width={300}
-            height={300}
+            width={400}
+            height={400}
           />
-          <div className="flex gap-4 font-bold items-center max-md:flex-col text-center my-4">
-            <label>Name:</label>
-            <h2>{product.name}</h2>
-          </div>
-          <div className="flex gap-4 max-md:flex-col text-center my-4">
-            <label>Description:</label>
-            <p>{product.description}</p>
-          </div>
-          <div className="flex gap-4 items-center max-md:flex-col text-center my-4">
-            <label htmlFor="price">Price:</label>
-            <p>${product.price}</p>
-          </div>
-          <div className="flex gap-2 mb-4 justify-self-center">
-            <label htmlFor="rating">Rating</label>
-            <Rating name="rating" value={product.rating} readOnly />
-          </div>
-          <div>
-            <AddToCart
-              id={product.id}
-              name={product.name}
-              image={product.image}
-              price={product.price}
-            />
-          </div>
+          <aside>
+            <div className="flex gap-4 items-center max-lg:flex-col text-center my-4 min-xl:w-[300px]">
+              <label className="font-bold">Name:</label>
+              <h2>{product.name}</h2>
+            </div>
+            <div className="flex gap-4 items-center max-lg:flex-col text-center my-4 min-xl:w-[300px]">
+              <label className="font-bold" htmlFor="mainCategory">
+                Main Category:
+              </label>
+              <p>{product?.main_category}</p>
+            </div>
+            <div className="flex gap-4 items-center max-lg:flex-col text-center my-4 min-xl:w-[300px]">
+              <label className="font-bold" htmlFor="subCategory">
+                Sub Category:
+              </label>
+              <p>{product?.sub_category}</p>
+            </div>
+            <div className="flex gap-4 max-lg:flex-col max-lg:text-center my-4 min-lg:w-[300px]">
+              <label className="font-bold">Description:</label>
+              <p>{product?.description}</p>
+            </div>
+            <div className="flex gap-4 items-center max-lg:flex-col text-center my-4 min-xl:w-[300px]">
+              <label className="font-bold" htmlFor="price">
+                Price:
+              </label>
+              <p>${product.price}</p>
+            </div>
+            <div className="flex gap-4 items-center max-lg:flex-col text-center my-4 min-xl:w-[300px]">
+              <label className="font-bold" htmlFor="rating">
+                Rating:
+              </label>
+              <Rating name="rating" value={product.rating} readOnly />
+            </div>
+          </aside>
+        </div>
+        <div className="my-4">
+          <AddToCart
+            id={product.id}
+            name={product.name}
+            image={product.image}
+            price={product.price}
+          />
         </div>
         <Feedbacks
           feedbacks={feedbacks ?? []}
           users={users.filter((u): u is NonNullable<typeof u> => u !== null)}
         />
-        <Link
-          className="text-blue-500 hover:text-blue-700"
-          href={`/products/${product.main_category.toLocaleLowerCase()}`}
-        >
-          ... Back to Products
-        </Link>
-      </div>
+      </section>
+      <Link
+        className="border block w-[20%] max-md:w-[50%] m-auto mt-4 rounded p-2 my-2 hover:bg-blue-700 hover:text-white self-center text-center"
+        href={`/products/${product.main_category.toLocaleLowerCase()}`}
+      >
+        ... Back to Products
+      </Link>
     </div>
   );
 };
