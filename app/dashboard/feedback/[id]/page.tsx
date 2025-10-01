74d3dae4847d3a34562a0805340a51e8165c44af
Alexander Hergert <alexander.hergert1989@gmail.com>
2025-10-01 16:12:53 +0200

    Change styles in /dashboard/feedback/id to match the styles from /dashboard/products/id.

--- app/dashboard/feedback/[id]/page.tsx
+++ app/dashboard/feedback/[id]/page.tsx
@@ -104,95 +104,87 @@ const FeedbackDetailsPage = async ({ params }: { params: Params }) => {
   return (
     <div>
       <h1 className="text-2xl my-4 text-center font-bold">Feedback Details</h1>
-      <div className="flex flex-col items-center border rounded-xl p-4 shadow-md md:min-w-[800px] md:w-1/2 m-auto max-md:w-[80%]">
-        <div className="flex gap-4 items-center max-md:flex-col text-center">
-          <label className="text-xl w-[200px] max-md:text-center">Title:</label>
-          <h2 className="w-[300px] max-md:text-center">{feedback?.title}</h2>
-        </div>
-        <div className="flex gap-4 items-center max-md:flex-col text-center my-4">
-          <label className="text-xl w-[200px] max-md:text-center">
-            Username:
-          </label>
-          {isAdmin ? (
-            <Link
-              href={`/dashboard/customers/${feedback?.userId}`}
-              className="w-[300px] max-md:text-center text-blue-500"
-            >
-              {userName}
-            </Link>
-          ) : (
-            <p className="w-[300px] max-md:text-center">{userName}</p>
-          )}
-        </div>
-        <Link
-          href={
-            isAdmin
-              ? `/dashboard/products/${feedback?.productId}`
-              : typeof product === "object" &&
-                product !== null &&
-                "main_category" in product
-              ? `/products/${product?.main_category.toLocaleLowerCase()}/${
-                  feedback?.productId
-                }`
-              : ""
-          }
-          className="flex gap-4 items-center max-md:flex-col text-center my-4"
-        >
-          <label className="text-xl w-[200px] max-md:text-center">
-            Product Id:
-          </label>
-          <p className="text-blue-500 w-[300px] max-md:text-center">
-            {feedback?.productId}
-          </p>
-        </Link>
-        <div className="flex gap-4 items-center max-md:flex-col text-center my-4">
-          <label className="text-xl w-[200px] max-md:text-center">
-            Product Name:
-          </label>
-          <p className="w-[300px] max-md:text-center">
-            {/* Type Guards */}
-            {typeof product === "object" &&
+      <section className="flex flex-col items-center border rounded-xl p-4 shadow-md md:min-w-[800px] md:w-1/3 m-auto max-md:w-[80%]">
+        <div className="flex max-md:gap-4 gap-8 max-lg:flex-col">
+          <Image
+            className="rounded-xl w-[400px] h-[400px] object-cover my-4"
+            width={400}
+            height={400}
+            src={
+              typeof product === "object" &&
               product !== null &&
               "name" in product &&
-              product?.name}
-            {typeof product === "object" &&
+              product?.image
+                ? product.image
+                : "/placeholder.png"
+            }
+            alt={
+              typeof product === "object" &&
               product !== null &&
-              "main_category" in product &&
-              ` (${product?.main_category})`}
-          </p>
-        </div>
-        <Image
-          className="rounded-xl w-[250px] h-[250px] object-cover my-4"
-          width={250}
-          height={250}
-          src={
-            typeof product === "object" &&
-            product !== null &&
-            "name" in product &&
-            product?.image
-              ? product.image
-              : "/placeholder.png"
-          }
-          alt={
-            typeof product === "object" &&
-            product !== null &&
-            "name" in product &&
-            product?.name
-              ? product.name
-              : "Product Image"
-          }
-        />
-        <div className="flex gap-4 items-center max-md:flex-col text-center">
-          <label className="text-xl w-[200px] max-md:text-center">
-            Comment:
-          </label>
-          <p className="w-[300px] max-md:text-center">{feedback?.comment}</p>
-        </div>
-        <div className="flex gap-4 items-center max-md:flex-col text-center my-4">
-          <label className="text-xl w-[200px] max-md:text-center">Date:</label>
-          <p className="w-[300px] max-md:text-center">
-            {feedback?.createdAt.toDateString()}
-          </p>
+              "name" in product &&
+              product?.name
+                ? product.name
+                : "Product Image"
+            }
+          />
+          <aside>
+            <div className="flex gap-4 max-lg:flex-col max-lg:text-center my-4 min-lg:w-[300px]">
+              <label className="font-bold">Title:</label>
+              <h2>{feedback?.title}</h2>
+            </div>
+            <div className="flex gap-4 max-lg:flex-col max-lg:text-center my-4 min-lg:w-[300px]">
+              <label className="font-bold">Username:</label>
+              {isAdmin ? (
+                <Link
+                  href={`/dashboard/customers/${feedback?.userId}`}
+                  className="max-md:text-center text-blue-500"
+                >
+                  {userName}
+                </Link>
+              ) : (
+                <p>{userName}</p>
+              )}
+            </div>
+            <Link
+              href={
+                isAdmin
+                  ? `/dashboard/products/${feedback?.productId}`
+                  : typeof product === "object" &&
+                    product !== null &&
+                    "main_category" in product
+                  ? `/products/${product?.main_category.toLocaleLowerCase()}/${
+                      feedback?.productId
+                    }`
+                  : ""
+              }
+              className="flex gap-4 max-lg:flex-col max-lg:text-center my-4 min-lg:w-[300px]"
+            >
+              <label className="font-bold">Product Id:</label>
+              <p className="text-blue-500">{feedback?.productId}</p>
+            </Link>
+            <div className="flex gap-4 max-lg:flex-col max-lg:text-center my-4 min-lg:w-[300px]">
+              <label className="font-bold">Product Name:</label>
+              <p>
+                {/* Type Guards */}
+                {typeof product === "object" &&
+                  product !== null &&
+                  "name" in product &&
+                  product?.name}
+                {typeof product === "object" &&
+                  product !== null &&
+                  "main_category" in product &&
+                  ` (${product?.main_category})`}
+              </p>
+            </div>
+            <div className="flex gap-4 max-lg:flex-col max-lg:text-center my-4 min-lg:w-[300px]">
+              <label className="font-bold">Comment:</label>
+              <p>{feedback?.comment}</p>
+            </div>
+            <div className="flex gap-4 max-lg:flex-col max-lg:text-center my-4 min-lg:w-[300px]">
+              <label className="font-bold">Date:</label>
+              <p>{feedback?.createdAt.toDateString()}</p>
+            </div>
+          </aside>
         </div>
         <Link
           href={`/dashboard/feedback/${feedback?.id}/edit_feedback`}
@@ -201,7 +193,7 @@ const FeedbackDetailsPage = async ({ params }: { params: Params }) => {
           Edit
         </Link>
         <DeleteFeedback id={feedback?.id || ""} />
-      </div>
+      </section>
       <Link
         className="border block w-[20%] max-md:w-[50%] m-auto mt-4 rounded p-2 my-2 hover:bg-blue-700 hover:text-white self-center text-center"
         href="/dashboard/feedback"
