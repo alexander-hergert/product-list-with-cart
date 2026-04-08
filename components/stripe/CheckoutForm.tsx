1f352f57006955c23ff664d9df20fe5502fcc860
Alexander Hergert <alexander.hergert1989@gmail.com>
2026-04-08 13:54:49 +0200

    Add environment variable to stripe payment confirmation page.

--- components/stripe/CheckoutForm.tsx
+++ components/stripe/CheckoutForm.tsx
@@ -39,7 +39,7 @@ export default function CheckoutForm({ dpmCheckerLink }: CheckoutFormProps) {
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: "http://localhost:3000/payment",
+        return_url: process.env.STRIPE_RETURN_URL!,
       },
     });
 
